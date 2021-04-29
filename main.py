df97b52011eb9df311012e160ca387ac51168b91
Nahum Maciel <nahumamaciel@gmail.com>
2021-04-29 05:17:00 -0700

    Added paste catch function, but date format needs fixing.

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ import tkinter as tk
 BG = '#1b2537'
 
 WINDOW = tk.Tk()
+# WINDOW.withdraw()
 WINDOW.configure(bg=BG)
 WINDOW.title('Time Calculator')
 
@@ -81,18 +82,30 @@ class Hours_Calculator_Form:
         def hours_calcualtor(self):
             start_parsed = datetime.strptime(
                 self.ent_start.ent.get(),
-                "%m/%d/%Y %H:%M"
+                "%m/%d/%Y   %H:%M"
             )
             print(start_parsed)
             end_parsed = datetime.strptime(
                 self.ent_end.ent.get(),
-                "%m/%d/%Y %H:%M"
+                "%m/%d/%Y   %H:%M"
             )
             interval_delta = end_parsed - start_parsed
             total_hours = (interval_delta.days * 24) + interval_delta.seconds / 3600
             self.lbl_display.lbl["text"] = '{0:.2f} hrs'.format(total_hours)
 
         def calcualtor_caller(self, event):
+            self.hours_calcualtor()
+
+        def get_paste_interval(self, event):
+            input_start_interval = WINDOW.clipboard_get().split("\t")[0]
+            input_end_interval = WINDOW.clipboard_get().split("\t")[1]
+
+            self.ent_start.ent.delete(0, tk.END)
+            self.ent_end.ent.delete(0, tk.END)
+
+            self.ent_start.ent.insert(0, input_start_interval)
+            self.ent_end.ent.insert(0, input_end_interval)
+
             self.hours_calcualtor()
 
         def __init__(
@@ -124,7 +137,7 @@ class Hours_Calculator_Form:
             )
 
             WINDOW.bind('<Return>', self.calcualtor_caller)
-
+            WINDOW.bind('<<Paste>>', self.get_paste_interval)
             self.ent_start = ent_start
             self.ent_end = ent_end
 
